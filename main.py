a5948faf1fdd5dd0a2ffda46ce4fb3697f541a8c
j4kov <j4kovlev.vyach@yandex.ru>
2025-02-24 23:14:39 +0300

    Add file

--- main.py
+++ main.py
@@ -143,5 +143,10 @@ def success():
     return 'успех'
 
 
+@app.route('/training/<prof>')
+def training(prof):
+    return render_template('training.html', prof=prof)
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1', debug=True)
